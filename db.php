0056f99409135be6202144d0e2588afa20b27071
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-12-19 11:52:25 +0630

    Post CRUD

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
-<?php
-
-// error_reporting(0);
+<?php 
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'root'); // xampp: '' (empty)
-define('DB_NAME', 'online_class');
-define('DB_PORT', 8889); // xampp: 3306
+define('DB_PASS', 'root');
+define('DB_NAME', 'php_crud');
+define('DB_PORT', 8889); // default 3306
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 if(mysqli_connect_error()) {
-    die('DB connet fail.');
-} 
+    die('DB connection fail.');
+}
